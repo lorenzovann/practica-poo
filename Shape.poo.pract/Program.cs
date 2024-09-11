bf5d65b31b1235557e3a68d9265bfbcbd3e683e8
walter lorenzo vannoni martinez <lorenzovannoni87@gmail.com>
2024-09-11 09:39:46 -0400

    Shape

---
+++ Shape.poo.pract/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shape.poo.pract
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // Crear un array de formas
+            Shape[] shapes = new Shape[]
+            {
+            new Rectangle(10, 5),
+            new Triangle(10, 5),
+            new Circle(7)
+            };
+
+           
+            double[] areas = new double[shapes.Length];
+
+            // Calcular las áreas y almacenarlas en la matriz
+            for (int i = 0; i < shapes.Length; i++)
+            {
+                areas[i] = shapes[i].CalculateSurface();
+            }
+
+            
+            for (int i = 0; i < areas.Length; i++)
+            {
+                Console.WriteLine($"Área de la forma {i + 1}: {areas[i]}");
+            }
+        }
+    }
+}
+
---
+++ Shape.poo.pract/Shape.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shape.poo.pract
+{
+    public abstract class Shape
+    {
+        protected double Width;
+        protected double Height;
+
+        protected Shape(double width, double height) 
+        { 
+            Width = width;
+            Height = height;
+              
+        }
+
+
+        public abstract double CalculateSurface();
+    }
+}
